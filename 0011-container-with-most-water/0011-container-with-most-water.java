2963a56edbd473065d826639a7625d8af25fa7d3
Nitin Kash9086-github <224430046+nitinkk0312-jpg@users.noreply.github.com>
2026-08-20 21:25:50 +0530

    Time: 5 ms (82.93%), Space: 77.3 MB (69.49%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,28 +1,25 @@
 class Solution {
     public int maxArea(int[] height) {
-
-        int n = height.length;
-        //l is the left pointer 
-        //r is the right pointer
-        int l = 0, r = n - 1;
-        int maxarea = 0;
-        while (l < r) {
-
-            int h = Math.min(height[l], height[r]);
-            int w = r - l;
-            int area = h * w;
-            maxarea = Math.max(area, maxarea);
-            if (height[l] <= height[r]) {
+        int n=height.length;
+        int l=0,r=n-1;
+        int maxarea=0;
+        while(l<r){
+            int h=Math.min(height[l],height[r]);
+            int w=r-l;
+            int area=h*w;
+            maxarea=Math.max(area,maxarea);
+            if(height[l]<=height[r]){
                 l++;
-            } else {
+            }
+            else{
                 r--;
             }
         }
-        return maxarea;
+       return maxarea; 
     }
 
     public void main(String[] args) {
-        int[] height = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+        int[] height = { 1, 8, 6, 2, 5, 8, 3, 7 };
         maxArea(height);
     }
 }
